454e7337fcecfa95f8b9d5cb230c481dc07a71e3
ailiersg <ailiersg@163.com>
2023-05-04 14:56:15 +0800

    add mysql

--- bot/index.ts
+++ bot/index.ts
@@ -1,63 +1,88 @@
 import dotenv from "dotenv";
 import { Bot, Context, GrammyError, HttpError, Middleware, MiddlewareFn, session } from "grammy";
-import { conversations, createConversation } from "@grammyjs/conversations";
+import {
+  type Conversation,
+  type ConversationFlavor,
+  conversations,
+  createConversation,
+} from "@grammyjs/conversations";
 import handleStart from "./start";
 
-// Use this token to access the HTTP API:
-// 6228171766:AAHjdZ7s9v1qT9LgQpFvJ52vFQpXnwZXLCU
+type MyContext = Context & ConversationFlavor;
+type MyConversation = Conversation<MyContext>;
 
-// import {
-//   startPaymentProcess,
-//   checkTransaction,
-// } from "./bot/handlers/payment.js";
-// import handleStart from "./bot/handlers/start.js";
+async function greeting(conversation: MyConversation, ctx: MyContext) {
+  // TODO: 编写对话
+}
+async function movie(conversation: MyConversation, ctx: MyContext) {
+  await ctx.reply("你有多少部最喜欢的电影？");
+  const count = await conversation.form.number();
+  const movies: string[] = [];
+  for (let i = 0; i < count; i++) {
+    await ctx.reply(`告诉我第 ${i + 1} 名！`);
+    const titleCtx = await conversation.waitFor(":text");
+    movies.push(titleCtx.msg.text);
+  }
+  await ctx.reply("这里有一个更好的排名！");
+  movies.sort();
+  await ctx.reply(movies.map((m, i) => `${i + 1}. ${m}`).join("\n"));
+}
 
 export async function initBot() {
-    // console.log("process.env.BOT_TOKEN",process.env.BOT_TOKEN)
-    try {
-    const bot = new Bot(process.env.BOT_TOKEN!,{client:{
-        timeoutSeconds:5,
-        sensitiveLogs:true
-    }});
+  try {
 
-    console.log("BOT_TOKEN",process.env.BOT_TOKEN)
-    // 用"你好！"来回复任意信息
-    // 你现在可以在你的 bot 对象 `bot` 上注册监听器。
-    // 当用户向你的 bot 发送消息时，grammY 将调用已注册的监听器。
+    const bot = new Bot(process.env.BOT_TOKEN!, {
+      client: {
+        timeoutSeconds: 5,
+        sensitiveLogs: true
+      }
+    });
+    // 安装会话插件。
+    bot.use(session({
+      initial() {
+        // 暂时返回一个空对象
+        return {};
+      },
+    }) as any);
+
+    // 安装对话插件。
+    bot.use((conversations()) as MiddlewareFn );
+    bot.use((createConversation(movie)) as Middleware<Context>);
+
+    console.log("BOT_TOKEN", process.env.BOT_TOKEN)
 
     // 处理 /start 命令。
-    bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-    // 处理其他的消息。
-    bot.on("message", (ctx) => ctx.reply("Got another message!"));
-
-    console.log('isInited',bot.isInited()) 
- // 错误处理
-    bot.catch((err) => {
-        const ctx = err.ctx; 
-        console.error(`Error while handling update ${ctx.update.update_id}:`);
-        const e = err.error;
-        if (e instanceof GrammyError) {
-          console.error("Error in request:", e.description);
-        } else if (e instanceof HttpError) {
-          console.error("Could not contact Telegram:", e);
-        } else {
-          console.error("Unknown error:", e);
-        }
-      });
-    // console.log('bot', bot)
-  
-// console.log("await bot.init()",await bot.botInfo)
-
-//  console.log("await bot.init()",await bot.init())
-
-    // 启动 bot。
-  
-   
-    bot.start({
-        timeout:10
+    bot.command("start", async (ctx:any) => {
+      await ctx.conversation.enter("movie");
     });
-    console.log("bot.api.getMe()",await bot.api.getMyCommands()) 
-   } catch (error) {
-    console.log('error',error)
-   }
+    // 处理其他的消息。
+    bot.on("message", (ctx) => {
+      console.log('msg', ctx.message)
+      ctx.reply("Got another message!")
+    });
+
+
+    // 错误处理
+    bot.catch((err) => {
+      const ctx = err.ctx;
+      console.error(`Error while handling update ${ctx.update.update_id}:`);
+      const e = err.error;
+      if (e instanceof GrammyError) {
+        console.error("Error in request:", e.description);
+      } else if (e instanceof HttpError) {
+        console.error("Could not contact Telegram:", e);
+      } else {
+        console.error("Unknown error:", e);
+      }
+    });
+
+
+    bot.start({
+      timeout: 10
+    });
+
+    console.log("bot.api.getMe()", await bot.api.getMyCommands())
+  } catch (error) {
+    console.log('error', error)
+  }
 }
--- controller/test.ts
+++ controller/test.ts
@@ -1,5 +1,21 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { myQuery } from "../mysql/queryClass";
+import { AllQueryStr } from '../mysql/queryStr';
 
-export function test1(request:FastifyRequest, reply:FastifyReply){
+export async function test1(request:FastifyRequest, reply:FastifyReply){
+    myQuery.query("select userid from user where id = ?",[1])
+    let allQueryStr = new AllQueryStr(myQuery)
+    let { queryWhereStr, queryData, isExist, where } =allQueryStr.getQueryWhereStr(
+        [
+            {
+                key: 'account',
+                act: '=',
+                val: 'asd',
+            },
+        ]
+    )
+
+    let d = await myQuery.query(`SELECT *
+    FROM admin ${where} ${queryWhereStr}`, queryData)
     reply.send(request.params)
 }
