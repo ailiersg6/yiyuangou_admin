673212b06d7a0f744c82dc68b3ceff2e10f6ea38
ailiersg <ailiersg@163.com>
2023-06-10 17:47:14 +0800

    1

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import fastify from 'fastify'
 import { bot_seting, inster1, test1 } from './controller/test'
 import { routes } from './routers/botRouer'
 import { routes1 } from './routers/apiRouter'
-import {initBot} from './bot/index'
+import { initBot } from './bot/index'
 import { Bot, webhookCallback } from 'grammy'
 const fastifyCors = require('fastify-cors');
 // env环境变量设置↓↓↓↓↓↓↓↓↓↓
@@ -24,52 +24,52 @@ server.setErrorHandler((error, request, reply) => {
 
   // 返回一个友好的错误响应
   reply.status(500).send({ error: 'Internal Server Error' });
-  
+
 });
 // 创建全局参数
 
 declare global {
-  var env:{
-     OWNER_WALLET: string; // 合约地址，它将接受所有付款
-     TONCENTER_TOKEN:string; // api key
-     BOT_LINK:string; // 机器人跳转链接
-     BOT_NAME:string; // 机器人名
-     BOT_TOKEN:string; //  Telegram Bot 令牌
-     NETWORK:'mainnet'; // 主网
-     MNEMONIC:string; // 钱包助记词
+  var env: {
+    OWNER_WALLET: string; // 合约地址，它将接受所有付款
+    TONCENTER_TOKEN: string; // api key
+    BOT_LINK: string; // 机器人跳转链接
+    BOT_NAME: string; // 机器人名
+    BOT_TOKEN: string; //  Telegram Bot 令牌
+    NETWORK: 'mainnet'; // 主网
+    MNEMONIC: string; // 钱包助记词
   }
 }
- bot_seting().then(data=>{
-  console.log("data",data)
-  if(!data){
-    console.log("无法获取设置",data)
-    return 
+bot_seting().then(data => {
+  console.log("data", data)
+  if (!data) {
+    console.log("无法获取设置", data)
+    return
   }
-  
-global.env.OWNER_WALLET = data.OWNER_WALLET;
-global.env.TONCENTER_TOKEN = data.TONCENTER_TOKEN;
-global.env.BOT_LINK = data.BOT_LINK;
-global.env.BOT_NAME = data.BOT_NAME;
-global.env.BOT_TOKEN = data.BOT_TOKEN;
-global.env.NETWORK = data.NETWORK;
-global.env.MNEMONIC = data.MNEMONIC;
 
-console.log("global.str1",global.env)
+  global.env.OWNER_WALLET = data.OWNER_WALLET;
+  global.env.TONCENTER_TOKEN = data.TONCENTER_TOKEN;
+  global.env.BOT_LINK = data.BOT_LINK;
+  global.env.BOT_NAME = data.BOT_NAME;
+  global.env.BOT_TOKEN = data.BOT_TOKEN;
+  global.env.NETWORK = data.NETWORK;
+  global.env.MNEMONIC = data.MNEMONIC;
 
- })
+  console.log("global.str1", global.env)
+
+  // 接收转账 
+
+  inster1()
+
+  initBot() // 初始化机器人
+})
 
 
 
-  
 
 
 
 
-// 接收转账 
 
-inster1()
-
-initBot() // 初始化机器人
 
 server.register(fastifyCors, {
   origin: '*',
@@ -78,10 +78,10 @@ server.register(fastifyCors, {
 
 server.register(routes)
 server.register(routes1)
-server.listen({ port: 8080,host:"0.0.0.0" }, (err, address) => {
-    if (err) {
-        console.error(err)
-        process.exit(1)
-    }
-    console.log(`Server listening at ${address}  `)
+server.listen({ port: 8080, host: "0.0.0.0" }, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}  `)
 })
